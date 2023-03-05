6571ffa0a0a1e066fd704766864764d0409ef0b7
Administrator <1726349213@qq.com>
2023-03-05 12:03:43 +0800

    项目初始化

---
+++ ape-user/src/main/java/com/hx/user/UserApplication.java
@@ -0,0 +1,11 @@
+package com.hx.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
---
+++ ape-user/src/main/java/com/hx/user/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.hx.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "测试";
+    }
+}
